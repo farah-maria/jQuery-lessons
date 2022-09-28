4b89fc502689161bb6be673147119b2b0b37d322
Farah M <farah.maria.rahman@gmail.com>
2022-09-28 11:49:55 +0000

    use jQ to access odd and even items in list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 
 
-//$("h1").hide();     JQ by element
-//$(".class").hide();   JQ by class
-//$("h2#heading1").hide(); JQ by id
-//$("p span").hide();  JQ by element and child
+//$("h1").hide();  //   JQ by element
+//$(".class").hide(); //   JQ by class
+//$("h2#heading1").hide(); // JQ by id
+//$("p span").hide(); // JQ by element and child
 
 //$("p span").css("color", "red"); JQ accessing CSS tags
 
-$('ul#list li').css('color', 'red');
-$('ul#list li:first').css('color', 'blue'); // JQ accessing first item in list
-$('ul#list li:last').css('color', 'green'); // JQ accessing last item in list
-
+//$('ul#list li').css('color', 'red');
+//$('ul#list li:first').css('color', 'blue'); // JQ accessing first item in list
+//$('ul#list li:last').css('color', 'green'); // JQ accessing last item in list
+//$('ul#list li:odd').css('font-size', 'large') // JQ accessing odd items in list (can also do even)
